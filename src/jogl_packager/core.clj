036fee6e587e4b8769e923e99ccd5839e008c82d
Jukka Villstedt <jukka.villstedt@gmail.com>
2013-12-26 20:33:48 +0200

    initial

---
+++ src/jogl_packager/core.clj
@@ -0,0 +1,51 @@
+(ns jogl-packager.core
+  (:require [clojure.java.shell :as shell])
+  (:import [java.io File]))
+
+(defn package-natives [source-file-prefix target-folder]
+  (for [[source-file-post-fix target-native-folder] [["macosx-universal"
+                                                      "macosx/X86_64"]]]
+    (let [full-target-native-folder (str target-folder "/native/" target-native-folder)]
+      (shell/sh "mkdir" "-p" full-target-native-folder)
+      (shell/sh "7z"
+                "x"
+                (str source-file-prefix "-natives-" source-file-post-fix ".jar")
+                (str "-o" full-target-native-folder))
+      (shell/sh "rm" "-r" (str full-target-native-folder "/META-INF")))))
+
+(defn package-module [package-folder module-name]
+  (let [target-folder (str package-folder "/" module-name)]
+
+    (println "packaging " module-name)
+    (shell/sh "mkdir" target-folder)
+    (shell/sh "7z" "x" (str package-folder "/jogamp-all-platforms/jar/" module-name ".jar") (str "-o" package-folder "/" module-name))
+    (package-natives (str package-folder "/jogamp-all-platforms/jar/" module-name)
+                     target-folder)
+    (shell/sh "zip" "-r" (str module-name ".jar") "." "-i" "*" :dir target-folder)
+
+    (println "installing " module-name)
+    (shell/sh "mvn" "install:install-file" (str "-Dfile=" target-folder "/" module-name ".jar") (str "-DpomFile=" module-name "-pom.xml"))))
+
+
+(defn package []
+  (let [package-folder "temp"]
+
+    (when (.exists (File. package-folder))
+      (shell/sh "rm" "-r" package-folder))
+
+    (shell/sh "mkdir" package-folder)
+
+    (println "downloading jogl")
+    (shell/sh "curl"
+              "http://jogamp.org/deployment/jogamp-current/archive/jogamp-all-platforms.7z"
+              "-o"
+              (str package-folder "/jogamp-all-platforms.7z"))
+
+    (println "extracting jogl")
+    (shell/sh "7z"
+              "x"
+              (str package-folder "/jogamp-all-platforms.7z")
+              (str "-o" package-folder))
+
+    (package-module package-folder "gluegen-rt")
+    (package-module package-folder "jogl-all")))
---
+++ test/jogl_packager/core_test.clj
@@ -0,0 +1,7 @@
+(ns jogl-packager.core-test
+  (:require [clojure.test :refer :all]
+            [jogl-packager.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
